0cb8b34fcdd946d98b5865e25fedd29de13a1d7c
Dani <drbrejnak@gmail.com>
2025-02-12 14:43:13 -0500

    SideMenu work

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Drawer, Button, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material/';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import { Drawer, Button, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider, Typography } from '@mui/material/';
+import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRounded';
+import HomeIcon from '@mui/icons-material/Home';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 export default function SideMenu() {
     const [open, setOpen] = React.useState(false);
@@ -12,25 +13,26 @@ export default function SideMenu() {
 
     const DrawerList = (
         <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-          {/* <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <List sx={{mt:'10vh', mb:'1vh'}}>
+            <Typography sx={{textAlign: 'center', fontSize:'2rem'}}>The King's Restaurant</Typography>
+            {['Dashboard', 'FAQ Page'].map((text, index) => (
               <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                <ListItemButton sx={{mt:'1vh'}}>
+                  <ListItemIcon sx={{display: 'flex', justifyContent: 'right'}}>
+                    {index % 2 === 0 ? <HomeIcon /> : <HelpOutlineIcon />}
                   </ListItemIcon>
-                  <ListItemText primary={text} />
+                  <ListItemText primary={text} sx={{textAlign: 'center'}} />
                 </ListItemButton>
               </ListItem>
             ))}
           </List>
-          <Divider /> */}
+          <Divider />
         </Box>
       );
 
   return (
     <>
-        <Button onClick={toggleDrawer(true)}>Open drawer</Button>
+        <Button onClick={toggleDrawer(true)}>Account Info&nbsp;<ArrowCircleRightRoundedIcon /></Button>
         <Drawer open={open} onClose={toggleDrawer(false)}>
             {DrawerList}
         </Drawer>
